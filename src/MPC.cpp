dbbb22baf2dcee3a41ff2549dab2a14f4ea941bf
yossico <yossicohen19@gmail.com>
2017-09-26 15:07:26 +0300

    bug fixing

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -119,30 +119,29 @@ MPC::~MPC() {}
 vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   bool ok = true;
   size_t i;
+
+  typedef CPPAD_TESTVECTOR(double) Dvector;
   const double x = state[0];
   const double y = state[1];
   const double psi = state[2];
   const double v = state[3];
   const double cte = state[4];
   const double epsi = state[5];
-  typedef CPPAD_TESTVECTOR(double) Dvector;
 
   // TODO: Set the number of model variables (includes both states and inputs).
-  // For example: If the state is a 4 element vector, the actuators is a 2
-  // element vector and there are 10 timesteps. The number of variables is:
-  //
-  // 4 * 10 + 2 * 9
+  // For example: If the state is a 6 element vector, the actuators is a 2
+  // element vector and there are 10 timesteps. The number of variables is: / 6 * 10 + 2 * 9
   size_t n_vars = N * 6 + (N - 1) * 2;
   // Set the number of constraints
   size_t n_constraints = N * 6;
+  std::cout << "state  coeffs " << state << coeffs << std::endl;
 
   // Initial value of the independent variables. SHOULD BE 0 besides initial state.
   Dvector vars(n_vars);
   for (int i = 0; i < n_vars; i++) {
     vars[i] = 0;
   }
-
-
+  
   // TODO: Set lower and upper limits for variables.
   Dvector vars_lowerbound(n_vars);
   Dvector vars_upperbound(n_vars);
