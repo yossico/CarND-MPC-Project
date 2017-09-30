94963530884d118aa3f69377b4b723c9ccc8e992
yossico <yossicohen19@gmail.com>
2017-09-30 08:52:49 +0300

    2nd review fix7

--- src/MPC.h
+++ src/MPC.h
@@ -12,6 +12,16 @@ using namespace std;
 #define BOUND 1.0e19 // Bound value for other variables
 #define NUMBER_OF_STEPS 10
 
+
+// Set weights parameters for the cost function
+#define W_CTE 8.4
+#define W_EPSI 0.32
+#define W_V 0.261
+#define W_DELTA 600000
+#define W_A 17.1
+#define W_DDELTA 0.01
+#define W_DA 0.00001
+
 class MPC {
  public:
   MPC();
