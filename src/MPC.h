1dc4c8c12621bdfa893ad119de7e34af0877fba9
yossico <yossicohen19@gmail.com>
2017-10-01 07:33:19 +0300

    fix 12

--- src/MPC.h
+++ src/MPC.h
@@ -19,10 +19,10 @@ using namespace std;
 #define REF_V 77.5
 
 // Set weights parameters for the cost function
-#define W_CTE 8.4
-#define W_EPSI 0.32
-#define W_V 0.261
-#define W_DELTA 600000
+#define W_CTE 500
+#define W_EPSI 500
+#define W_V 20
+#define W_DELTA 60
 #define W_A 17.1
 #define W_DDELTA 0.01
 #define W_DA 0.00001
