60353569167e27bf8ae02aba4d594c4e418e5b10
root <yossicohen19@gmail.com>
2017-10-03 05:27:49 +0000

    finally working!

--- src/MPC.h
+++ src/MPC.h
@@ -12,16 +12,16 @@ using namespace std;
 #define LF 2.67
 #define REF_CTE 0
 #define REF_EPSI 0
-#define REF_V 50
+#define REF_V  30 
 
 // Set weights parameters for the cost function
-#define W_CTE 8.4
-#define W_EPSI 0.32
-#define W_V 0.261
-#define W_DELTA 600000
-#define W_A 17.1
+#define W_CTE 30
+#define W_EPSI 10
+#define W_V  20
+#define W_DELTA 100000
+#define W_A 20
 #define W_DDELTA 0.01
-#define W_DA 0.00001
+#define W_DA 10
 
 
 // Set lower and upper limits for variables.
